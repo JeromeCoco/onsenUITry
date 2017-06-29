49267e57937c666444e79342d17b55baee090cb8
Jerome Coco <jeromcoco1@gmail.com>
2017-06-29 14:00:12 +0800

    new sidenavbar

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,15 +1,13 @@
-document.addEventListener('init', function(event)
+window.fn = {};
+window.fn.open = function()
 {
-    var page = event.target;
-    if (page.id === 'page1')
-    {
-        page.querySelector('#push-button').onclick = function()
-        {
-            document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
-        }
-    }
-    else if (page.id === 'page2')
-    {
-        page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-    }
-});
+    document.getElementById('menu').open();
+};
+
+window.fn.load = function(page)
+{
+    var content = document.getElementById('content');
+    var menu = document.getElementById('menu');
+    content.load(page)
+        .then(menu.close.bind(menu));
+};
